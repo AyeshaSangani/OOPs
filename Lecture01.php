c01abf4aa220719202202275359fe0ecb4f6937f
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-03-17 04:30:58 +0500

    oops

---
+++ Lecture01.php
@@ -0,0 +1,80 @@
+<?php
+
+class  Update1 {
+        
+    public $name;
+
+    private function xyz(){
+        
+        echo $this->name = "Ali";
+        }
+
+        function __construct()
+        {
+            $this->xyz();
+        }
+
+}
+
+$abc = new Update1 ();
+
+echo $xyz->xyz(); 
+
+
+
+// !!!!!!!!!!!!!!!!!!!
+
+
+class  Update2 {
+        
+    public $name;
+
+    public function ab (){
+        
+        echo $this->name = "Ali";
+        }
+
+}
+
+$abc = new Update2 ();
+echo $abc->ab(); 
+
+
+
+
+// !!!!!!!!!!!!!!!!
+
+
+
+class  Updates {
+        
+    public $name;
+
+    public function abc (){
+        $this->name = "John";
+        echo $this->name;
+        }
+
+}
+
+$Update = new Updates ();
+$Update ->abc();
+
+
+class  Update {
+        
+    public $name;
+
+    public function abc (){
+        $this->name = "Ahmed";
+        echo $this->name;
+        }
+
+}
+
+$Update = new Update ();
+$Update ->abc(); 
+
+
+
+?>
