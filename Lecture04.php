e56e748eb0b0c7b760932b1a3ec1c27cfe1eb3ee
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-03-27 03:40:29 +0500

    Inheritance...

---
+++ Lecture04.php
@@ -0,0 +1,30 @@
+<?php
+
+   Encapsulation
+  
+class Demo1(
+
+public $name;
+
+
+public function display(){
+
+
+echo $this->name = "Ahmed";
+
+
+
+}
+
+
+}
+
+
+$demo1= new Demo1();
+
+$demo1->display();
+
+
+class Demo2()
+
+?>
