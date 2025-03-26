62329fefa4aac33e3017a3c486712f0286ca33d1
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-03-27 03:30:58 +0500

    Encapsulation...

--- Lecture03.php
+++ Lecture03.php
@@ -1,3 +1,59 @@
 <?php
 
+           WHEN PROPERTY AND METHOD BOTH ARE PUBLIC
+
+class Update {
+
+//property
+
+
+public $name;
+
+//method
+
+public function abc(){
+
+
+$this->name = "Ahmed";
+
+
+echo $this->name;
+
+}
+}
+
+
+this is object
+
+
+
+$Update new Update();
+
+$Update->abc()
+
+
+
+
+            WHEN PROPERTY IS PUBLC AND METHOD OR FUNCTION IS PRIVATE
+
+class Update {
+
+public $name;
+
+private function abc (){
+
+echo $this->name = "Ali";
+
+}
+
+function _construct()
+
+$this->abc();
+
+$abc new Update();
+
+ //echo $abc->abc()
+
+
+
 ?>
