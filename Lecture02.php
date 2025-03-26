995b019644837c94cf95f1252d1a5f54d332623c
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-03-27 03:16:09 +0500

    Encapsulation...

--- Lecture02.php
+++ Lecture02.php
@@ -1,2 +1,107 @@
 <?php
+
+ // Encapsulation
+
+
+class Random{
+
+    // property
+    
+    public $username = "Ali";
+
+   
+
+
+}
+
+$obj = new Random();
+echo $obj->username
+
+
+
+
+
+
+// class Random{
+
+//     // property
+    
+//     private $username = "Ali";
+
+//     //  Method - function
+
+//     private function abc(){
+//     echo $this->username;
+//     }
+
+
+// }
+
+// $obj = new Random();
+// echo $obj->abc()
+
+
+
+
+
+
+// class Random{
+
+//     // property
+    
+//     private $username = "Ali";
+
+//     //  Method - function
+
+//     private function abc(){
+//     echo $this->username;
+//     }
+
+
+// //  constructor
+// public function __construct(){
+
+//   $this->abc();
+
+// }
+
+// }
+
+
+
+// $obj = new Random();
+
+
+
+
+// class Rough{
+
+//     // property
+    
+//     public $username = "Ali <br>";
+
+//     //  Method - function
+
+//     public function abc(){
+//     echo $this->fullname = "Ali Ahmed <br>";
+//     }
+
+
+// //  constructor
+// public function __construct(){
+
+//   echo $this->fathername = "Usman Ali <br>";
+
+// }
+
+// }
+
+
+
+// $obj = new Rough();
+// echo $obj->username;
+// echo $obj->abc()
+
+
+
 ?>
